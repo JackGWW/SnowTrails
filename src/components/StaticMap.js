783bfd13f153790177e7b164f05269731ecb3dc9
Jack Williams <jack@jackw.ca>
2020-12-06 13:11:06 -0800

    Add higher resolution static map

--- src/components/StaticMap.js
+++ src/components/StaticMap.js
@@ -5,25 +5,35 @@ import ImageZoom from 'react-native-image-pan-zoom';
 const styles = StyleSheet.create({
   container: {
   },
+  windowWidth: Dimensions.get('window').width,
+  windowHeight: Dimensions.get('window').height,
   map: {
-    width: 3182,
-    height: 2021
+    // width: 3182,
+    // height: 2021,
+    width: 7991,
+    height: 5079,
   }
 });
 
 export default function StaticMap() {
 
   return (
-    <ImageZoom cropWidth={Dimensions.get('window').width}
-      cropHeight={Dimensions.get('window').height}
+    <ImageZoom
+      enableCenterFocus={false}
+      cropWidth={styles.windowWidth}
+      cropHeight={styles.windowHeight}
       imageWidth={styles.map.width}
       imageHeight={styles.map.height}
-      enableCenterFocus={false}
-      minScale={0.1}
-      maxScale={2}
+      // minScale={0.12}
+      // maxScale={2}
+      minScale={0.05}
+      maxScale={1}
     >
-      <Image style={styles.map}
-        source={require('../../assets/trailMap.png')} />
+      <Image
+        style={styles.map}
+        //source={require('../../assets/trailMap.png')}
+        source={require('../../assets/trailMapLarge.png')}
+      />
     </ImageZoom>
   )
 }
