ca436eab555c5f0eab727c10e77c4ec2f0da2eab
Jack Williams <jack@jackw.ca>
2020-12-06 19:41:49 -0500

    Implement marker sizing based on zoom level

--- src/components/trails/markers/CircleMarker.js
+++ src/components/trails/markers/CircleMarker.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Circle30 from "./Circle30"
 import Circle40 from "./Circle40"
+import Circle50 from "./Circle50"
 
 export default circleMarker = (props) => {
-    //Update to switch based on zoom level
-    console.log(props.longitudeDelta)
-    if (props.longitudeDelta) {
-        return (
-            <Circle40 location={props.location} />
-        );
+    //As the screen zooms out, make the icons smaller
+    const delta = props.longitudeDelta
+    console.log(delta)
+    switch (true) {
+        case (delta < 0.009):
+            console.log(50)
+            return <Circle50 location={props.location} />
+        case (delta < 0.0105):
+            console.log(40)
+            return <Circle40 location={props.location} />
+        default:
+            console.log(30)
+            return <Circle30 location={props.location} />
     }
 };
 
