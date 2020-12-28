db54550e9f094d7560ba4c4bb24fad665437bc0e
Jack Williams <jack@jackw.ca>
2020-12-27 21:24:38 -0500

    Add trail markers

--- src/components/trails/EnchantedForest.js
+++ src/components/trails/EnchantedForest.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Polyline } from "react-native-maps";
+import CircleMarker from "./markers/CircleMarker"
 import trail from "../../../data/json/EnchantedForest.json"
 
 
 const ShowTrail = (props) => {
+  const childRef = useRef();
+
   return (
-    <Polyline
-      coordinates={trail}
-      strokeColor={"yellow"}
-      strokeWidth={3}
-    />
+    <>
+      <CircleMarker
+        longitudeDelta={props.longitudeDelta}
+        location={{ latitude: 44.51374, longitude: -80.35033 }}
+        trailName={"Enchanted Forest"}
+      />
+      <CircleMarker
+        longitudeDelta={props.longitudeDelta}
+        location={{ latitude: 44.51455, longitude: -80.35657 }}
+        trailName={"Enchanted Forest"}
+        ref={childRef}
+      />
+      <CircleMarker
+        longitudeDelta={props.longitudeDelta}
+        location={{ latitude: 44.51337, longitude: -80.35663 }}
+        trailName={"Enchanted Forest"}
+      />
+      <Polyline
+        coordinates={trail}
+        strokeColor={"yellow"}
+        strokeWidth={3}
+        tappable={true}
+        onPress={() => childRef.current.displayTrailName()}
+      />
+    </>
   );
 };
 
--- src/components/trails/ForestGump.js
+++ src/components/trails/ForestGump.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Polyline } from "react-native-maps";
+import CircleMarker from "./markers/CircleMarker"
 import trail from "../../../data/json/ForestGump.json"
 
 
 const ShowTrail = (props) => {
+  const childRef = useRef();
+
   return (
-    <Polyline
-      coordinates={trail}
-      strokeColor={"black"}
-      strokeWidth={3}
-    />
+    <>
+      <CircleMarker
+        longitudeDelta={props.longitudeDelta}
+        location={{ latitude: 44.51376, longitude: -80.35361 }}
+        trailName={"Forest Gump"}
+        ref={childRef}
+      />
+      <Polyline
+        coordinates={trail}
+        strokeColor={"black"}
+        strokeWidth={3}
+        tappable={true}
+        onPress={() => childRef.current.displayTrailName()}
+      />
+    </>
   );
 };
 
