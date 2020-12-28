a93456c42af8d47f38dfa4e3475bde24d63df9a1
Jack Williams <jack@jackw.ca>
2020-12-27 21:57:04 -0500

    Add trail markers

--- src/components/trails/FarmRoad.js
+++ src/components/trails/FarmRoad.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Polyline } from "react-native-maps";
+import CircleMarker from "./markers/CircleMarker"
 import trail from "../../../data/json/FarmRoad.json"
 
 
 const ShowTrail = (props) => {
+  const childRef = useRef();
+
   return (
-    <Polyline
-      coordinates={trail}
-      strokeColor={"black"} 
-      strokeWidth={3}
-    />
+    <>
+      <CircleMarker
+        longitudeDelta={props.longitudeDelta}
+        location={{ latitude: 44.51194, longitude: -80.35205 }}
+        trailName={"Farm Road"}
+        ref={childRef}
+      />
+      <Polyline
+        coordinates={trail}
+        strokeColor={"black"}
+        strokeWidth={3}
+        tappable={true}
+        onPress={() => childRef.current.displayTrailName()}
+      />
+    </>
   );
 };
 
--- src/components/trails/RobinsRun.js
+++ src/components/trails/RobinsRun.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Polyline } from "react-native-maps";
+import CircleMarker from "./markers/DiamondMarker"
 import trail from "../../../data/json/RobinsRun.json"
 
 
 const ShowTrail = (props) => {
+  const childRef = useRef();
+
   return (
-    <Polyline
-      coordinates={trail}
-      strokeColor={"yellow"}
-      strokeWidth={3}
-    />
+    <>
+      <CircleMarker
+        longitudeDelta={props.longitudeDelta}
+        location={{ latitude: 44.51504, longitude: -80.35914 }}
+        trailName={"Robin's Run"}
+        ref={childRef}
+      />
+      <Polyline
+        coordinates={trail}
+        strokeColor={"yellow"}
+        strokeWidth={3}
+        tappable={true}
+        onPress={() => childRef.current.displayTrailName()}
+      />
+    </>
   );
 };
 
