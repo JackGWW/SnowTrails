e1bf36ed2d7566af0e03f3eed584919c12ec5898
Jack Williams <jack@jackw.ca>
2021-01-01 16:55:36 -0500

    Add legend to map

--- src/components/LiveMap.js
+++ src/components/LiveMap.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Dimensions, StatusBar } from "react-native";
+import { StyleSheet, View, Dimensions, StatusBar, Image } from "react-native";
 import MapView, {
   UrlTile,
 } from "react-native-maps";
@@ -45,45 +45,54 @@ export default class LiveMap extends React.Component {
   }
 
   updateRegion(region) {
-    this.setState({longitudeDelta: region.longitudeDelta})
+    this.setState({ longitudeDelta: region.longitudeDelta })
   }
 
   render() {
     return (
       <View style={styles.container}>
         {
-          <MapView
-            initialCamera={this.state.initialCamera}
-            provider={"google"} // Always use GoogleMaps (instead of MapKit on iOS)
-            showsUserLocation={true}
-            showsCompass={true}
-            toolbarEnabled={false} // Hide map buttons on marker press
-            minZoomLevel={14}
-            maxZoomLevel={19}
-            mapType={"none"}
-            style={styles.mapStyle}
-            ref={(ref) => (this.mapView = ref)}
-            onMapReady={this.mapSetup.bind(this)} //Initialize map boundaries when the map loads
-            onRegionChangeComplete={region => this.updateRegion(region)}
-            mapPadding={{
-              top: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
-              right: 0,
-              bottom: 0,
-              left: 0
-            }}
-          >
+          <>
+            <MapView
+              initialCamera={this.state.initialCamera}
+              provider={"google"} // Always use GoogleMaps (instead of MapKit on iOS)
+              showsUserLocation={true}
+              showsCompass={true}
+              toolbarEnabled={false} // Hide map buttons on marker press
+              minZoomLevel={14}
+              maxZoomLevel={19}
+              mapType={"none"}
+              style={styles.mapStyle}
+              ref={(ref) => (this.mapView = ref)}
+              onMapReady={this.mapSetup.bind(this)} //Initialize map boundaries when the map loads
+              onRegionChangeComplete={region => this.updateRegion(region)}
+              mapPadding={{
+                top: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
+                right: 0,
+                bottom: 0,
+                left: 0
+              }}
+            >
 
-            <UrlTile
-              urlTemplate={
-                //"https://tile.thunderforest.com/landscape/{z}/{x}/{y}@2x.png?apikey=b5fc3b88cf204ad8b1381b659cc07391"
-                "https://api.mapbox.com/styles/v1/jackgww/ckixum56n651w19npcrja4rnq/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFja2d3dyIsImEiOiJja2l4dDZ5bnIxZTh1MnNwZmdxODA4cjU1In0.QruuU5HoAnwNtt0UE45GSg"
-              }
-              maximumZ={22}
-              flipY={false}
-              zIndex={-3}
-            />
-            <AllTrails longitudeDelta={this.state.longitudeDelta} />
-          </MapView>
+              <UrlTile
+                urlTemplate={
+                  //"https://tile.thunderforest.com/landscape/{z}/{x}/{y}@2x.png?apikey=b5fc3b88cf204ad8b1381b659cc07391"
+                  "https://api.mapbox.com/styles/v1/jackgww/ckixum56n651w19npcrja4rnq/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFja2d3dyIsImEiOiJja2l4dDZ5bnIxZTh1MnNwZmdxODA4cjU1In0.QruuU5HoAnwNtt0UE45GSg"
+                }
+                maximumZ={22}
+                flipY={false}
+                zIndex={-3}
+              />
+              <AllTrails longitudeDelta={this.state.longitudeDelta} />
+            </MapView>
+            <View style={styles.legendContainer}>
+              <Image
+                source={require('../../assets/legend.png')}
+                style={styles.legend}
+                resizeMode='contain'
+              />
+            </View>
+          </>
         }
       </View>
     );
@@ -98,6 +107,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   mapStyle: {
+    position: 'absolute',
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
   },
@@ -105,4 +115,24 @@ const styles = StyleSheet.create({
     flex: 1,
     position: "absolute",
   },
+  legendContainer: {
+    position: 'absolute',
+    left: 1,
+    right: 0,
+    bottom: 1,
+    height: 100,
+    width: 100,
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+  legend: {
+    top: 0,
+    right: 0,
+    height: 80,
+    width: 80,
+    flexDirection: 'row',
+  }
 });
