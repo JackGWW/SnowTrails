b6e23f1a515ed298915186485b165e511091f817
Jack Williams <jack@jackw.ca>
2021-01-10 17:37:19 -0800

    Add loading spinner for live map

--- src/components/LiveMap.js
+++ src/components/LiveMap.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { StyleSheet, View, Dimensions, StatusBar, Image } from "react-native";
-import MapView, {
-  UrlTile,
-} from "react-native-maps";
+import MapView, { UrlTile } from "react-native-maps";
+import Spinner from "react-native-loading-spinner-overlay";
 
 // Polyline components for all trails
-import AllTrails from "./trails/AllTrails"
+import AllTrails from "./trails/AllTrails";
 
 export default class LiveMap extends React.Component {
   constructor() {
@@ -27,12 +26,15 @@ export default class LiveMap extends React.Component {
     };
     this.state = {
       initialCamera: initialCamera,
-      longitudeDelta: 0.011
+      longitudeDelta: 0.011,
+      spinner: true,
     };
   }
 
   mapSetup() {
     // Set map boundaries to the area containing the trails
+    this.setState({ spinner: false });
+
     northEastLimit = {
       latitude: 44.531269,
       longitude: -80.334765,
@@ -45,55 +47,57 @@ export default class LiveMap extends React.Component {
   }
 
   updateRegion(region) {
-    this.setState({ longitudeDelta: region.longitudeDelta })
+    this.setState({ longitudeDelta: region.longitudeDelta });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        {
-          <>
-            <MapView
-              initialCamera={this.state.initialCamera}
-              provider={"google"} // Always use GoogleMaps (instead of MapKit on iOS)
-              showsUserLocation={true}
-              showsCompass={true}
-              toolbarEnabled={false} // Hide map buttons on marker press
-              minZoomLevel={14}
-              maxZoomLevel={18}
-              mapType={"none"}
-              style={styles.mapStyle}
-              ref={(ref) => (this.mapView = ref)}
-              onMapReady={this.mapSetup.bind(this)} //Initialize map boundaries when the map loads
-              onRegionChangeComplete={region => this.updateRegion(region)}
-              mapPadding={{
-                top: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
-                right: 0,
-                bottom: 0,
-                left: 0
-              }}
-            >
-
-              <UrlTile
-                urlTemplate={
-                  //"https://tile.thunderforest.com/landscape/{z}/{x}/{y}@2x.png?apikey=b5fc3b88cf204ad8b1381b659cc07391"
-                  "https://api.mapbox.com/styles/v1/jackgww/ckixum56n651w19npcrja4rnq/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFja2d3dyIsImEiOiJja2l4dDZ5bnIxZTh1MnNwZmdxODA4cjU1In0.QruuU5HoAnwNtt0UE45GSg"
-                }
-                maximumZ={22}
-                flipY={false}
-                zIndex={-3}
-              />
-              <AllTrails longitudeDelta={this.state.longitudeDelta} />
-            </MapView>
-            <View style={styles.legendContainer}>
-              <Image
-                source={require('../../assets/legend.png')}
-                style={styles.legend}
-                resizeMode='contain'
-              />
-            </View>
-          </>
-        }
+        <Spinner
+          visible={this.state.spinner}
+          textContent={"Loading..."}
+          textStyle={styles.spinnerTextStyle}
+          animation={"fade"}
+          overlayColor={"rgba(0, 0, 0, 0.5)"}
+        />
+        <MapView
+          initialCamera={this.state.initialCamera}
+          provider={"google"} // Always use GoogleMaps (instead of MapKit on iOS)
+          showsUserLocation={true}
+          showsCompass={true}
+          toolbarEnabled={false} // Hide map buttons on marker press
+          minZoomLevel={14}
+          maxZoomLevel={18}
+          mapType={"none"}
+          style={styles.mapStyle}
+          ref={(ref) => (this.mapView = ref)}
+          onMapReady={this.mapSetup.bind(this)} //Initialize map boundaries when the map loads
+          onRegionChangeComplete={(region) => this.updateRegion(region)}
+          mapPadding={{
+            top: Platform.OS === "ios" ? 20 : StatusBar.currentHeight,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          <UrlTile
+            urlTemplate={
+              //"https://tile.thunderforest.com/landscape/{z}/{x}/{y}@2x.png?apikey=b5fc3b88cf204ad8b1381b659cc07391"
+              "https://api.mapbox.com/styles/v1/jackgww/ckixum56n651w19npcrja4rnq/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFja2d3dyIsImEiOiJja2l4dDZ5bnIxZTh1MnNwZmdxODA4cjU1In0.QruuU5HoAnwNtt0UE45GSg"
+            }
+            maximumZ={22}
+            flipY={false}
+            zIndex={-3}
+          />
+          <AllTrails longitudeDelta={this.state.longitudeDelta} />
+        </MapView>
+        <View style={styles.legendContainer}>
+          <Image
+            source={require("../../assets/legend.png")}
+            style={styles.legend}
+            resizeMode="contain"
+          />
+        </View>
       </View>
     );
   }
@@ -107,7 +111,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   mapStyle: {
-    position: 'absolute',
+    position: "absolute",
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
   },
@@ -116,23 +120,26 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   legendContainer: {
-    position: 'absolute',
+    position: "absolute",
     left: 1,
     right: 0,
     bottom: 1,
     height: 100,
     width: 100,
     flex: 1,
-    alignItems: 'center',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    backgroundColor: 'transparent'
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "transparent",
   },
   legend: {
     top: 0,
     right: 0,
     height: 80,
     width: 80,
-    flexDirection: 'row',
-  }
+    flexDirection: "row",
+  },
+  spinnerTextStyle: {
+    color: "#FFF",
+  },
 });
