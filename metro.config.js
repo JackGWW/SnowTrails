2ab41b66dc89c1668fe716aa20b3e0d5aec906a1
Jack Williams <jack@jackw.ca>
2025-11-09 12:32:26 -0800

    Fix GeoJSON import configuration
    Change .geojson extension from assetExts to sourceExts to allow proper module imports in Metro bundler.

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,7 @@ const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 // This replaces `const config = getDefaultConfig(__dirname);`
 const config = getSentryExpoConfig(__dirname);
 
-// Add support for .geojson files
-config.resolver.assetExts.push('geojson');
+// Add support for .geojson files as source files (not assets)
+config.resolver.sourceExts.push('geojson');
 
 module.exports = config;
